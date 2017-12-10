31facc836427b814ce57519abb149edc806f9d6a
Flashk <mevengar@gmail.com>
2017-12-10 20:01:05 +0100

    Added comments on interface
    Added comments for interface methods.

--- jsonobjects/src/main/java/json/destiny2/mappers/Mappeable.java
+++ jsonobjects/src/main/java/json/destiny2/mappers/Mappeable.java
@@ -5,6 +5,19 @@ import json.destiny2.model.response.ResponseObject;
 
 public interface Mappeable<T> {
 
+	/**
+	 * Generates a ResponseObject for the generic type T using data from the input json.
+	 * @param json
+	 * @return
+	 * @throws Exception
+	 */
 	ResponseObject<T> getObjectFromJson(String json) throws Exception;
+	
+	/**
+	 * Generates a ResponseList for the generic type T using data from the input json.
+	 * @param json
+	 * @return
+	 * @throws Exception
+	 */
 	ResponseList<T> getListFromJson(String json) throws Exception;
 }
